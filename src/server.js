b344bed2aa07628cb0bb44b3b3418e8f41f3fe2f
purna <p.purnababu@gmail.com>
2024-12-20 15:24:46 +0530

    update

--- src/server.js
+++ src/server.js
@@ -2,9 +2,9 @@ let express = require('express');
 
 let app = express();
 
-app.use(express.static(__dirname+'/dist/kofejob-angular'));
+app.use(express.static(__dirname+'/dist/edon-ng'));
 
 app.get('/*',(req,resp)=>{
-    resp.sendFile(__dirname+'/dist/kofejob-angular/index.html');
+    resp.sendFile(__dirname+'/dist/edon-ng/index.html');
 });
 app.listen(process.env.PORT || 4200);
